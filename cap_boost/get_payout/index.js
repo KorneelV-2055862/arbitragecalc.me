8205d4b5146d8e49899fa82a48c281bbb1b4b2bf
KorneelV-2055862 <71925980+KorneelV-2055862@users.noreply.github.com>
2022-12-14 15:35:52 +0100

    Create index.js

---
+++ cap_boost/get_payout/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("#calculate_btn").click(function () {
+
+        event.preventDefault();
+        if (!$("#form").valid()) return;
+        var boost_cap = $("#boost_cap").val();
+        var boostOdd = $("#boosted_odd").val();
+        var baseOdd = $("#base_odd").val();
+        var stake = $("#stake").val();
+        var stakeBoost = Math.min(boost_cap, stake)
+        var payoutBoost = stakeBoost * boostOdd
+        var stakeNormal = stake - stakeBoost
+        var payoutNormal = stakeNormal * baseOdd
+        var payout = payoutBoost + payoutNormal
+
+        $("#total_payout").text("Total Payout: €" + payout)
+        $("#result_boost").text("Boost stake: €" + stakeBoost + " with odd: " + boostOdd + " has payout: €" + payoutBoost)
+        $("#result_base").text("Normal stake: €" + stakeNormal + " with odd: " + baseOdd + " has payout: €" + payoutNormal)
+
+    });
+});
